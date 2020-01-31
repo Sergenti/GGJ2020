2f4857c55772143621da3111977d4658f4d7a59d
Florian <florian.rieder@unil.ch>
2020-01-31 23:33:11 +0100

    start of background

---
+++ GGJ2020/Assets/BackgroundManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundManager : MonoBehaviour
+{
+    public BoxCollider2D spawnZone;
+    public BoxCollider2D despawnZone;
+
+    public GameObject star;
+
+    public void GenerateRandomStar()
+    {
+        Vector3 spawnPoint = Utilities.RandomPointInBounds(spawnZone.bounds);
+        Instantiate(star, spawnPoint, Quaternio.down)
+
+    }
+}
