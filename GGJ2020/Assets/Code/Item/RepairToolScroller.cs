a75e310195b9af17ad8fc7b7f92a5dadb241bd85
Florian <florian.rieder@unil.ch>
2020-02-01 16:51:22 +0100

    fixed item scrolling with gamepad

--- GGJ2020/Assets/Code/Item/RepairToolScroller.cs
+++ GGJ2020/Assets/Code/Item/RepairToolScroller.cs
@@ -7,14 +7,12 @@ namespace Code.Item
     // Scroll through the toolList and raise 3 toolEvent, previous,current and next, tool
     public class RepairToolScroller : MonoBehaviour
     {
-        [Space,Header("Events")]
         [SerializeField] private ToolEvent previousToolEvent;
         [SerializeField] private ToolEvent currentToolEvent;
         [SerializeField] private ToolEvent nextToolEvent;
-        [Space]
         [SerializeField] private RepairToolList toolList;
-        [SerializeField] private float cooldown = 0.1f;
-        private float cooldownTimer = 0f;
+        private bool canScrollUp = true;
+        private bool canScrollDown = true;
 
         private int _toolListIdx = 0;
 
@@ -24,40 +22,38 @@ namespace Code.Item
 
         void Update()
         {
-            //Force to select an item at the beginning of thw game
             if (!hadPrintOnce)
             {
-                if(toolList.GetRepairTool(0) == null){return;}
+                if (toolList.GetRepairTool(0) == null) { return; }
                 currentToolEvent.Raise(toolList.GetRepairTool(0));
                 hadPrintOnce = true;
             }
 
-            // use cooldown to prevent too fast scrolling using the joystick (cause its triggers are axis input)
-            if (cooldownTimer >= cooldown)
+            // check if joystick triggers have been released
+            if (Input.GetAxisRaw("ItemScrollUp") == 0 && !canScrollUp)      canScrollUp = true;
+            if (Input.GetAxisRaw("ItemScrollDown") == 0 && !canScrollDown)  canScrollDown = true;
+
+            // check if player has pressed an item scroll button
+            if (Input.GetButtonDown("ItemScrollUp") || (Input.GetAxisRaw("ItemScrollUp") == 1 && canScrollUp))
             {
-                if (Input.GetButtonDown("ItemScrollUp") || Input.GetAxisRaw("ItemScrollUp") == 1)
-                {
-                    _toolListIdx = (_toolListIdx + 1) % toolList.GetListSize();
-                    currentToolEvent.Raise(toolList.GetRepairTool(_toolListIdx));
-                }
-                else if (Input.GetButtonDown("ItemScrollDown") || Input.GetAxisRaw("ItemScrollDown") == 1)
-                {
-                    _toolListIdx = (_toolListIdx - 1);
+                _toolListIdx = (_toolListIdx + 1) % toolList.GetListSize();
+                currentToolEvent.Raise(toolList.GetRepairTool(_toolListIdx));
 
-                    if (_toolListIdx < 0)
-                    {
-                        _toolListIdx = toolList.GetListSize() - 1;
-                    }
-
-                    currentToolEvent.Raise(toolList.GetRepairTool(_toolListIdx));
-                }
-
-                // reset timer
-                cooldownTimer = 0f;
+                canScrollUp = false;
             }
-            
-            // increment timer
-            cooldownTimer += Time.deltaTime;
+            else if (Input.GetButtonDown("ItemScrollDown") || (Input.GetAxisRaw("ItemScrollDown") == 1 && canScrollDown))
+            {
+                _toolListIdx = (_toolListIdx - 1);
+
+                if (_toolListIdx < 0)
+                {
+                    _toolListIdx = toolList.GetListSize() - 1;
+                }
+
+                currentToolEvent.Raise(toolList.GetRepairTool(_toolListIdx));
+
+                canScrollDown = false;
+            }
         }
     }
 }
--- GGJ2020/Assets/UIButtonSelector.cs
+++ GGJ2020/Assets/UIButtonSelector.cs
@@ -7,14 +7,14 @@ using UnityEngine.UI;
 public class UIButtonSelector : MonoBehaviour, ISelectHandler// required interface when using the OnSelect method.
 {
     [SerializeField] private GameObject selector;
-    
+
     //Do this when the selectable UI object is selected.
     public void OnSelect(BaseEventData eventData)
     {
-        Debug.Log(this.gameObject.name + " was selected");
         DisplaySelector(true);
 
         // iterate through all objects that have the same parent as this one
+        // super ugly code
         foreach (Transform buttonTransform in transform.parent)
         {
             if (buttonTransform.gameObject == this.gameObject) continue;
