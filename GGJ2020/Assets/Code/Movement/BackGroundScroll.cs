9fbc60a75fb800eda6081dc80f64a8a90f56c621
Zacharie Tevaearai <zacharietevaearai@outlook.com>
2020-02-02 11:27:19 +0100

    Fixed issue,

--- GGJ2020/Assets/Code/Movement/BackGroundScroll.cs
+++ GGJ2020/Assets/Code/Movement/BackGroundScroll.cs
@@ -6,19 +6,21 @@ namespace Code.Movement
     {
         [SerializeField] private float verticalSpeed = 0.1f;
         [SerializeField] private float magicValue = 0.925f;
+
+        private float initialHeigh;
         // Start is called before the first frame update
         void Start()
         {
-        
+            initialHeigh = transform.position.y;
         }
 
         // Update is called once per frame
         void Update()
         {
             transform.Translate(Vector3.down*verticalSpeed*Time.deltaTime);
-            if (transform.position.y < magicValue)
+            if (transform.position.y < initialHeigh - magicValue)
             {
-                transform.SetPositionAndRotation(transform.position + new Vector3(0f, magicValue * 2, 0f),
+                transform.SetPositionAndRotation(transform.position + new Vector3(0f, magicValue * 2 + initialHeigh, 0f),
                     Quaternion.identity);
             }
         }
